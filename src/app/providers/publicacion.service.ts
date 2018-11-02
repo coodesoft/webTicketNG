823d674437b21099dd8c166ba40c77e575cf6057
lucho <lucho.2012.tandil@gmail.com>
2018-11-02 00:56:16 -0300

    [Slider principal] ya esta lo mas grueso de lo correspondiente a estilos, se agregaron imagenes de prueba

--- src/app/providers/publicacion.service.ts
+++ src/app/providers/publicacion.service.ts
@@ -11,19 +11,19 @@ export class PublicacionService {
   constructor() { }
 
   private superDestacadaDummy = [
-    {'id':1, 'img':'','title':'Transformers',     'imgPortada':''},
-    {'id':2, 'img':'','title':'Misión imposible', 'imgPortada':''},
-    {'id':3, 'img':'','title':'Dragon Ball Z',    'imgPortada':''},
-    {'id':4, 'img':'','title':'Relatos Salvajes', 'imgPortada':''},
-    {'id':5, 'img':'','title':'Madagascar',       'imgPortada':''}
+    {'id':1, 'img':'assets/test/matrix2.jpg','title':'Matrix',     'imgPortada':'assets/test/matrix1.jpg'},
+    {'id':2, 'img':'assets/test/mision2.jpg','title':'Misión imposible', 'imgPortada':'assets/test/mision1.jpg'},
+    {'id':3, 'img':'assets/test/dragon2.jpg','title':'Dragon Ball Z',    'imgPortada':'assets/test/dragon1.jpg'},
+    {'id':4, 'img':'assets/test/relato2.jpg','title':'Relatos Salvajes', 'imgPortada':'assets/test/relato1.jpg'},
+    {'id':5, 'img':'assets/test/mada2.jpg','title':'Madagascar 2',     'imgPortada':'assets/test/mada1.jpg'}
   ]; // [Test] Datos de prueba para maquetacion
 
   private destacadaDummy = [
     {'id':1, 'img':'','title':'Transformers',     'imgPortada':''},
-    {'id':2, 'img':'','title':'Misión imposible', 'imgPortada':''},
+    {'id':2, 'img':'','title':'Misión imposible', 'imgPortada':'test/mision1.jpg'},
     {'id':3, 'img':'','title':'Dragon Ball Z',    'imgPortada':''},
     {'id':4, 'img':'','title':'Relatos Salvajes', 'imgPortada':''},
-    {'id':5, 'img':'','title':'Madagascar',       'imgPortada':''}
+    {'id':5, 'img':'test/mada2.jpg','title':'Madagascar 2',     'imgPortada':'test/mada1.jpg'}
   ]; // [Test] Datos de prueba para maquetacion
 
 
@@ -32,6 +32,6 @@ export class PublicacionService {
   }
 
   getDestacadas(){
-    this.destacadasLoaded.next(this.destacadaDummy);
+    this.destacadasLoaded.next(this.superDestacadaDummy);
   }
 }
